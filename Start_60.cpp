77b5013011679e70c152e6ff519b0ea091fce3dc
Assaka <2682728781@qq.com>
2026-01-31 21:24:48 +0800

    hh

--- Start_60.cpp
+++ Start_60.cpp
@@ -3,24 +3,23 @@
 using namespace std;
 const int N = 2e5 + 9;
 int a[N];
-stack<int> st;
+int st[N];
+int ans[N];
+int top;
+
 int main(){
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
   int n;
   cin >> n;
   for(int i = 1; i <= n; i++) cin >> a[i];
-  int index = 1;
-  while (index <= n){
-    if(st.empty()) cout << -1 << ' ';
-    else if(st.top() < a[index]) {
-      cout << st.top() << ' ';
-    }else {
-      st.pop();
-      continue;
-    }
-    st.push(a[index]);
-    index++;
+  
+  for(int i = 1; i <= n; i++){
+    while (top && a[st[top]] >= a[i]) top--;
+    if(!top) ans[i] = -1;
+    else ans[i] = a[st[top]];
+    st[++top] = i;
   }
   
+  for(int i = 1; i <= n; i++) cout << ans[i] << ' ';
   return 0;
 }
