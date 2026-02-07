92245c8ef91e70e7ab4fbae6f5009b08facf7c6c
Assaka <2682728781@qq.com>
2026-02-07 21:44:12 +0800

    hh

---
+++ Star_42.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+const int N = 59;
+int fa[N];
+vector<int> g[N];
+int n;
+
+void dfs(int x){
+  cout << x << ' ';
+  for(auto &y : g[x]) dfs(y);
+}
+
+void bfs(int rt){
+  queue<int> q;
+  q.push(rt);
+  while(q.size()){
+    int x = q.front();
+    q.pop();
+    cout << x << ' ';
+    for(int &y : g[x]) q.push(y);
+  }
+}
+
+int main(){
+  ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+  cin >> n;
+  for(int i = 2; i <= n; i++) cin >> fa[i];
+  for(int i = 2; i <= n; i++) g[fa[i]].push_back(i);
+  for(int i = 1; i <= n; i++) sort(g[i].begin(), g[i].end());
+  dfs(1);
+  cout << '\n';
+  bfs(1);
+  return 0;
+}
