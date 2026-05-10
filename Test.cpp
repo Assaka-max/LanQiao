734d4e8138871907dc3bfcf48b7cfa3c622b2e93
Assaka <2682728781@qq.com>
2026-05-10 22:58:19 +0800

    hh

--- Test.cpp
+++ Test.cpp
@@ -1,23 +1,36 @@
 #include<iostream>
-#include<vector>
-#include<queue>
 using namespace std;
-const int N = 1e6 + 9;
-int a[N];
+const int N = 5e4 + 9;
+const int M = 5e4 + 9;
+int n, m;
+long long stone[N];
+
+long long check(long long mid){
+  long long res = 0;
+  long long pre = 0;
+  for(int i = 2; i <= n + 2; i++){
+    if(stone[i] - pre < mid){
+      res++;
+      continue;
+    }
+    pre = stone[i];
+  }
+  return res;
+}
 
 int main(){
   ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
-  int n, k; cin >> n >> k;
-  int ans = -1;
-  for(int i = 1; i <= n; i++) cin >> a[i];
-  int q[N];
-  int front = 1; int back = 0;
-  for(int i = 1; i <= n; i++){
-    if(back >= front && i - q[front] + 1 > k) front++;
-    while(a[q[back]] >= a[i]) back--;
-    back++; q[back] = i;
-    if(i >= k) ans = max(ans, a[q[back]] - a[q[front]]);
+  int L;
+  cin >> L >> n >> m;
+  stone[n+2] = L;
+  for(int i = 2; i <= n+1; i++) cin >> stone[i];
+  long long l = 1;
+  long long r = 1e9 + 9;
+  while(l + 1 != r){
+    long long mid = (l+r)/2;
+    if(check(mid) <= m) l = mid;
+    else r = mid;
   }
-  cout << ans;
+  cout << l;
   return 0;
 }
